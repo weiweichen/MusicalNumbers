224f4c10e5ad7c92ed76fcf0ef4c677f694a7eba
Weiwei Chen <weiwei.chen.uci@gmail.com>
2018-12-09 02:56:18 -0800

    Change number to note mapping.

--- src/main/scala/musicalnumbers/musicalnumbers.scala
+++ src/main/scala/musicalnumbers/musicalnumbers.scala
@@ -5,12 +5,15 @@ import scala.collection.immutable._
 
 object MusicalNumbers{
 
-  var firstNote : Option[Int] = None
+  var firstNumber = 0
 
   def main(args: Array[String]): Unit = {
     val inputName = args(0)
     val tuneType = args(1)
     val key = args(2)
+
+    firstNumber = getFirstNumber(args)
+
     val fis = utils.getFileInputStream(args)
 
     val tuneName = inputName.split('.').head.split('/').last.split('_').map(s => s(0).toUpper + s.tail).mkString(" ")
@@ -22,21 +25,20 @@ object MusicalNumbers{
     utils.writeToFile(outputName, List(header,body).mkString("\n"))
   }
 
+  def getFirstNumber(args:Array[String]) : Int = {
+    val fs = utils.getFileInputStream(args)
+    val firstNumber = getNextNumber(fs)
+    fs.close()
+    firstNumber
+  }
+
   def getNextNumber(fs: FileInputStream) : Int = {
     var ch : Char = 'a'
     while(!ch.isDigit){
       ch = fs.read().toChar
     }
 
-    val number = ch.toInt - 0x30
-
-    firstNote match {
-      case Some(_) =>
-      case None => firstNote = Some(number)
-    }
-
-    // (number + 10 - firstNote.get) % 10
-    number
+    ch.toInt - 0x30
   }
 
   val tuneInfo = Map(
@@ -46,29 +48,34 @@ object MusicalNumbers{
   )
 
 
-  val abcNotes = List('C', 'D', 'E', 'F', 'G', 'A', 'B')
+  val abcNotes =
+    List("C,", "D,", "E,", "F,", "G,", "A,", "B,",
+         "C", "D", "E", "F", "G", "A", "B",
+         "c", "d", "e", "f", "g", "a", "b")
+
   val digits = List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
   val keyIndex = Map(
-    "C" -> 0,
-    "D" -> 1,
-    "E" -> 2,
-    "F" -> 3,
-    "G" -> 4,
-    "A" -> 5,
-    "B" -> 6
+    "C" -> 7,
+    "D" -> 8,
+    "E" -> 9,
+    "F" -> 10,
+    "G" -> 11,
+    "A" -> 12,
+    "B" -> 13
   )
 
   def getNoteABCMapping(key: String) = {
     val rootIdx = keyIndex(key)
-    digits.foldLeft(Map[Int, Char]())((map: Map[Int, Char], number: Int) => {
-      val abcNoteIdx = (number + rootIdx) % abcNotes.size
-      map + (number -> abcNotes(abcNoteIdx))
+    digits.foldLeft(Map[Int, String]())((map: Map[Int, String], number: Int) => {
+      // val abcNoteIdx = (number + rootIdx) % abcNotes.size
+      val abcNoteIdx = number - firstNumber + rootIdx
+      map + (number  -> abcNotes(abcNoteIdx))
     })
   }
 
   val measureEnd = List(
     "|", "|", "|", "|\n",
-    "|", "|", "|", ":||\n|:",
+    "|", "|", "|", ":||\n",
     "|", "|", "|", "|\n",
     "|", "|", "|", ":|\n"
   )
@@ -89,25 +96,33 @@ object MusicalNumbers{
 
   def abcBody(tuneType: String, fs: FileInputStream, key: String) : String = {
     val notesPerMeasure = tuneInfo(tuneType)._6
-    val totalNumNotes = tuneInfo(tuneType)._5
     val noteABCMapping = getNoteABCMapping(key)
     println(s"Note to ABC Mapping\n${noteABCMapping.mkString("\n")}\n")
 
     val builder = new StringBuilder
 
     for(line <- 0 until 4) {
+      val lyrics = new StringBuilder
+      lyrics.append(s"w: ")
+      if (line == 2)
+        builder.append("|:")
       for(measure <- 0 until 4) {
         for (note <- 0 until notesPerMeasure / 2) {
           val number = getNextNumber(fs)
           builder.append(s"${noteABCMapping(number)}")
+          lyrics.append(s"${number} ")
         }
         builder.append(" ")
         for (note <- notesPerMeasure / 2 until notesPerMeasure) {
           val number = getNextNumber(fs)
           builder.append(s"${noteABCMapping(number)}")
+          lyrics.append(s"${number} ")
         }
         builder.append(measureEnd(line * 4 + measure))
+        lyrics.append(" ")
       }
+      lyrics.append("\n")
+      builder.append(lyrics.toString())
     }
     builder.toString()
   }
